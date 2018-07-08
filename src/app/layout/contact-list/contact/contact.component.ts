19b0cab47da22107fdae10c5f35ad9c587ce8de5
Przemyslaw Chudzinski <przemek@lig-it.com>
2018-07-08 23:26:38 +0200

    fixed bugs

--- src/app/layout/contact-list/contact/contact.component.ts
+++ src/app/layout/contact-list/contact/contact.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IUser } from '../../../auth/models/user.model';
-import { ActivatedRoute } from '@angular/router';
-import { tap, take } from 'rxjs/operators';
-import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,21 +7,8 @@ import { AuthService } from '../../../auth/auth.service';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
   @Input() contact: IUser;
-  show = true; // TODO: Return correctly data from server
 
-  constructor(private route: ActivatedRoute, private auth: AuthService) {}
-
-  ngOnInit() {
-    this.auth.user$
-      .pipe(
-        take(1),
-        tap(
-          user =>
-            user && (this.contact._id === user._id ? (this.show = false) : null)
-        )
-      )
-      .subscribe();
-  }
+  constructor() {}
 }
